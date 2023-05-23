3cb652e67b264563e2807b09d48dcdd929d9f576
Eduarda Souto <eduardasouto2823@gmail.com>
2023-05-22 21:19:34 -0300

    feat: conexão com back-end

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,21 +1,81 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, ActivityIndicator, Alert } from 'react-native';
 import { Button, Text } from 'react-native-elements';
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://todo-app-wx01.onrender.com",
+});
 
 export default function CategoryScreen({ navigation }) {
+  const [listas, setListas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchListas();
+  }, []);
+
+  const fetchListas = async () => {
+    try {
+      const response = await instance.get("list");
+      setListas(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const handleAdicionarLista = () => {
+    Alert.prompt('Adicionar Lista', 'Digite o nome da nova lista:', (nome) => {
+      if (nome) {
+        criarLista(nome);
+      }
+    });
+  };
+
+  const criarLista = async (name) => {
+    try {
+      setLoading(true);
+      const response = await instance.post("list", { name });
+      const novaLista = response.data;
+      setListas([...listas, novaLista]);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text h2 style={styles.title}>Suas Listas</Text>
-      <Button
-        title="Lista de Compras"
-        onPress={() => navigation.navigate('Lista', { category: 'Lista de Compras' })}
-        containerStyle={styles.buttonContainer}
-      />
-      <Button
-        title="ToDo List"
-        onPress={() => navigation.navigate('Lista', { category: 'ToDo List' })}
-        containerStyle={styles.buttonContainer}
-      />
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          {listas.length > 0 ? (
+            listas.map((lista) => (
+              <Button
+                key={lista.id}
+                title={lista.name}
+                onPress={() => navigation.navigate('Lista', { category: lista.name })}
+                containerStyle={styles.buttonContainer}
+              />
+            ))
+          ) : (
+            <View>
+              <Text style={styles.noListText}>Nenhuma lista encontrada.</Text>
+            </View>
+          )}
+          <Button
+            title="Adicionar Lista"
+            onPress={handleAdicionarLista}
+            color="#f194ff"
+            containerStyle={styles.buttonContainer}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -33,4 +93,7 @@ const styles = StyleSheet.create({
     width: '80%',
     marginBottom: 20,
   },
-}); 
+  noListText: {
+    marginBottom: 20,
+  },
+});
