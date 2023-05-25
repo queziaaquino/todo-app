fe1b8f72ca2c1d8bca608447d8e0381a0b2dc2c4
Eduarda Souto <eduardasouto2823@gmail.com>
2023-05-24 21:09:50 -0300

    modal para adicionar novas listas

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ActivityIndicator, Alert } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import { Button, Text } from 'react-native-elements';
-import axios from "axios";
+import axios from 'axios';
+import CustomInputDialogModal from './CustomInputDialogModal';
 
 const instance = axios.create({
-  baseURL: "https://todo-app-wx01.onrender.com",
+  baseURL: 'http://192.168.0.5:3000',
 });
 
-export default function CategoryScreen({ navigation }) {
+const CategoryScreen = ({ navigation }) => {
   const [listas, setListas] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [dialogVisible, setDialogVisible] = useState(false);
 
   useEffect(() => {
     fetchListas();
@@ -17,7 +20,7 @@ export default function CategoryScreen({ navigation }) {
 
   const fetchListas = async () => {
     try {
-      const response = await instance.get("list");
+      const response = await instance.get('list');
       setListas(response.data);
       setLoading(false);
     } catch (error) {
@@ -27,17 +30,18 @@ export default function CategoryScreen({ navigation }) {
   };
 
   const handleAdicionarLista = () => {
-    Alert.prompt('Adicionar Lista', 'Digite o nome da nova lista:', (nome) => {
-      if (nome) {
-        criarLista(nome);
-      }
-    });
+    setDialogVisible(true);
   };
 
-  const criarLista = async (name) => {
+  const handleDialogClose = () => {
+    setDialogVisible(false);
+  };
+
+  const handleDialogSubmit = async (nome) => {
+    setDialogVisible(false);
     try {
       setLoading(true);
-      const response = await instance.post("list", { name });
+      const response = await instance.post('list', { name: nome });
       const novaLista = response.data;
       setListas([...listas, novaLista]);
       setLoading(false);
@@ -49,7 +53,7 @@ export default function CategoryScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text h2 style={styles.title}>Suas Listas</Text>
+      <Text style={styles.title}>Suas Listas</Text>
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
@@ -76,9 +80,14 @@ export default function CategoryScreen({ navigation }) {
           />
         </>
       )}
+      <CustomInputDialogModal style={styles.teste}
+        visible={dialogVisible}
+        onClose={handleDialogClose}
+        onSubmit={handleDialogSubmit}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -88,6 +97,7 @@ const styles = StyleSheet.create({
   },
   title: {
     marginBottom: 30,
+    fontSize: 40
   },
   buttonContainer: {
     width: '80%',
@@ -96,4 +106,9 @@ const styles = StyleSheet.create({
   noListText: {
     marginBottom: 20,
   },
+  teste: {
+    alignItems: 'center'
+  }
 });
+
+export default CategoryScreen;
